d6fcc8fb396015230ad0ab6fdf73577b32f10301
penoplasttt <youkvakva@gmail.com>
2024-01-03 22:05:38 +0300

    fix handlers

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -5,25 +5,97 @@ import (
 
 	pb "github.com/penoplasttt/authService/protos/gen/go"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type Auth interface {
+	Login(ctx context.Context, email string, password string, appID int) (token string, err error)
+	RegisterNewUser(ctx context.Context, email string, password string) (userID int64, err error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
 
 type serverAPI struct {
 	pb.UnimplementedAuthServer
+	auth Auth
 }
 
 func Register(gRPC *grpc.Server) {
 	pb.RegisterAuthServer(gRPC, &serverAPI{})
 }
 
-func (s *serverAPI) Login (ctx context.Context, req *pb.LoginRequest)(*pb.LoginResponse,error){
-	return &pb.LoginResponse{Token: "123"}, nil
-	
+func (s *serverAPI) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
+
+	if err := validateLogin(req); err != nil {
+		return nil, err
+	}
+
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &pb.LoginResponse{Token: token}, nil
 }
 
-func (s *serverAPI) Register (ctx context.Context, req *pb.RegisterRequest)(*pb.RegisterResponse, error){
-	panic("implement me")
+func (s *serverAPI) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
+	if err := validateRegister(req); err != nil {
+		return nil, err
+	}
+
+	userID, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &pb.RegisterResponse{UserId: userID}, nil
 }
 
-func (s *serverAPI) IsAdmin(ctx context.Context, req *pb.IsAdminRequest)(*pb.IsAdminResponse, error){
-	panic("implement me")
+func (s *serverAPI) IsAdmin(ctx context.Context, req *pb.IsAdminRequest) (*pb.IsAdminResponse, error) {
+	if err := validateIsAdmin(req); err != nil {
+		return nil, err
+	}
+
+	isAdmin, err := s.auth.IsAdmin(ctx, req.GetUserId())
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &pb.IsAdminResponse{IsAdmin: isAdmin}, nil
+}
+
+func validateLogin(req *pb.LoginRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	if req.GetAppId() == 0 {
+		return status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
+	return nil
+}
+
+func validateRegister(req *pb.RegisterRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	return nil
+}
+
+func validateIsAdmin(req *pb.IsAdminRequest) error {
+	if req.GetUserId() == 0 {
+		return status.Error(codes.InvalidArgument, "userID is required")
+	}
+
+	return nil
 }
